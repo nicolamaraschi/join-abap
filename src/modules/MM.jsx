11c6ca893b3b3251cb818bed0f257a9f0e9893b0
Nicola Maraschi <nicola.maraschi01@gmail.com>
2025-06-13 14:54:17 +0200

    modif mm

--- src/modules/MM.jsx
+++ src/modules/MM.jsx
@@ -2,38 +2,221 @@ export const mmData = `
 ### Modulo MM (Materials Management)
 
 #### Material Master Data
+
 **MARA (General Material Data)**
+
 * Chiavi Primarie: \`MATNR\`
 * Descrizione: Dati generali del materiale (tipo materiale, gruppo merce, etc.)
 * Possibili Join:
+
   * **MAKT** (Material Descriptions): su \`MATNR, SPRAS\`
   * **MARM** (Units of Measure for Material): su \`MATNR\`
   * **MBEW** (Material Valuation): su \`MATNR\`
   * **MARD** (Storage Location Data for Material): su \`MATNR\`
+  * **MARC** (Material per Plant): su \`MATNR\`
+  * **MVKE** (Sales Data): su \`MATNR\`
+  * **MLGN** (Warehouse Data): su \`MATNR\`
+  * **MLAN** (Tax Data): su \`MATNR\`
+
+---
 
 **MAKT (Material Descriptions)**
+
 * Chiavi Primarie: \`MATNR, SPRAS\`
 * Descrizione: Descrizioni del materiale in diverse lingue.
 * Possibili Join:
+
   * **MARA** (General Material Data): su \`MATNR\`
 
-**MARM (Units of Measure for Material)**
-* Chiavi Primarie: \`MATNR, MEINH\`
-* Descrizione: Unità di misura alternative per il materiale (quantità, dimensioni, etc.).
+---
+
+**MARC (Material Master per Plant)**
+
+* Chiavi Primarie: \`MATNR, WERKS\`
+* Descrizione: Dati del materiale specifici per impianto (MRP, pianificazione, produzione, etc.)
 * Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+  * **MAPR** (Link MARC <=> PROP): su \`MATNR, WERKS\`
+  * **MDKP** (MRP Document Header): su \`MATNR, WERKS\`
+
+---
+
+**MVKE (Material Sales Data)**
+
+* Chiavi Primarie: \`MATNR, VKORG, VTWEG\`
+* Descrizione: Dati di vendita per il materiale (organizzazione di vendita, canale di distribuzione).
+* Possibili Join:
+
   * **MARA** (General Material Data): su \`MATNR\`
 
-**MBEW (Material Valuation)**
-* Chiavi Primarie: \`MATNR, BWKEY, BWTAR\`
-* Descrizione: Dati contabili e di valorizzazione del materiale.
-* Possibili Join:
-  * **MARA** (General Material Data): su \`MATNR\`
+---
 
 **MARD (Storage Location Data for Material)**
+
 * Chiavi Primarie: \`MATNR, WERKS, LGORT\`
 * Descrizione: Quantità a stock per magazzino.
 * Possibili Join:
+
   * **MARA** (General Material Data): su \`MATNR\`
+  * **MARC** (Material per Plant): su \`MATNR, WERKS\`
+
+---
+
+**MLGN (Material / Warehouse Number)**
+
+* Chiavi Primarie: \`MATNR, LGNUM\`
+* Descrizione: Assegnazione materiale a numero magazzino (WM).
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MLGT (Material / Storage Type)**
+
+* Chiavi Primarie: \`MATNR, LGNUM, LGTYP\`
+* Descrizione: Dettagli del materiale per tipo di stoccaggio (WM).
+* Possibili Join:
+
+  * **MLGN** (Warehouse Number): su \`MATNR, LGNUM\`
+
+---
+
+**MSKA (Sales Order Stock)**
+
+* Chiavi Primarie: \`MANDT, MATNR, WERKS, CHARG, SOBKZ, KDAUF, KDPOS\`
+* Descrizione: Stock speciale relativo ad ordini cliente.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MSPR (Project Stock)**
+
+* Chiavi Primarie: \`MANDT, MATNR, WERKS, CHARG, SOBKZ, PSPNR\`
+* Descrizione: Stock speciale per progetti.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MEAN (International Article Number)**
+
+* Chiavi Primarie: \`MATNR, EAN11\`
+* Descrizione: Numeri articolo internazionali (EAN/GTIN).
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**PGMI (Planning Material)**
+
+* Chiavi Primarie: \`MATNR\`
+* Descrizione: Materiale utilizzato nella pianificazione abbinata.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**PROP (Forecast Parameters)**
+
+* Chiavi Primarie: \`MATNR, WERKS\`
+* Descrizione: Parametri di previsione per il materiale.
+* Possibili Join:
+
+  * **MAPR** (Link con MARC): su \`MATNR, WERKS\`
+
+---
+
+**MAPR (Link MARC <=> PROP)**
+
+* Chiavi Primarie: \`MATNR, WERKS\`
+* Descrizione: Collegamento tra i dati impianto-materiale e i parametri di previsione.
+* Possibili Join:
+
+  * **MARC** (Material per Plant): su \`MATNR, WERKS\`
+  * **PROP** (Forecast Parameters): su \`MATNR, WERKS\`
+
+---
+
+**MBEW (Material Valuation)**
+
+* Chiavi Primarie: \`MATNR, BWKEY, BWTAR\`
+* Descrizione: Dati contabili e di valorizzazione del materiale.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MVER (Material Consumption)**
+
+* Chiavi Primarie: \`MATNR, WERKS\`
+* Descrizione: Consumi storici del materiale.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MLAN (Tax Data Material Master)**
+
+* Chiavi Primarie: \`MATNR, ALAND\`
+* Descrizione: Dati fiscali del materiale per paese.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MTQSS (Material master view: QM)**
+
+* Chiavi Primarie: \`MATNR, WERKS\`
+* Descrizione: Dati qualità per materiale/impianto.
+* Possibili Join:
+
+  * **MARC** (Material per Plant): su \`MATNR, WERKS\`
+
+---
+
+**MATERIALID (External Material Number with Version)**
+
+* Chiavi Primarie: \`MATNR, MATNR\_EXTERNAL, MATNR\_VERSION\`
+* Descrizione: Numerazione esterna e versionamento per il materiale.
+* Possibili Join:
+
+  * **MARA** (General Material Data): su \`MATNR\`
+
+---
+
+**MDTB (MRP Table)**
+
+* Chiavi Primarie: \`MDID, MDVL, MATNR, WERKS\`
+* Descrizione: Tabelle con i risultati della pianificazione fabbisogni.
+* Possibili Join:
+
+  * **MDKP** (Header per MRP): su \`MDID, MDVL\`
+  * **MARC** (Material per Plant): su \`MATNR, WERKS\`
+
+---
+
+**MDKP (MRP Document Header)**
+
+* Chiavi Primarie: \`MDID, MDVL\`
+* Descrizione: Header delle run MRP.
+* Possibili Join:
+
+  * **MDTB** (MRP Table): su \`MDID, MDVL\`
+
+---
+
+
+
 
 #### Purchasing
 
@@ -44,6 +227,10 @@ export const mmData = `
 * Possibili Join:
 
   * **EKPO** (Purchasing Document Item): su \`EBELN\`
+  * **EKPA** (Partner functions): su \`EBELN\`
+  * **EKAN** (Vendor address purchasing): su \`EBELN\`
+
+---
 
 **EKPO (Purchasing Document Item)**
 
@@ -54,11 +241,16 @@ export const mmData = `
   * **EKKO** (Purchasing Document Header): su \`EBELN\`
   * **EKET** (Scheduling Agreements): su \`EBELN, EBELP\`
   * **EINA** (Purchasing Info Record - General Data): su \`INFNR\`
+  * **EINE** (Purchasing Info Record - Purchasing Organization Data): su \`INFNR, EKORG, WERKS, ESOKZ\`
+  * **EORD** (Source list): su \`MATNR, WERKS, LIFNR\`
   * **EBAN** (Purchase Requisition): su \`BANFN, BNFPO\`
   * **EKPV** (Shipping-Specific Data on Stock Tfr. for Purch. Doc. Item): su \`EBELN, EBELP\`
   * **EKKN** (Account assignment in purchasing): su \`EBELN, EBELP\`
   * **EKES** (Order Acceptance/Fulfillment Confirmations): su \`EBELN, EBELP\`
   * **EIPO** (Item export / import data): su \`EBELN, EBELP\`
+  * **VETVG** (Delivery Due Index for Stock Transfer): su \`EBELN, EBELP\`
+
+---
 
 **EKET (Scheduling Agreements)**
 
@@ -68,6 +260,8 @@ export const mmData = `
 
   * **EKPO** (Purchasing Document Item): su \`EBELN, EBELP\`
 
+---
+
 **EKPV (Shipping-Specific Data on Stock Tfr. for Purch. Doc. Item)**
 
 * Chiavi Primarie: \`EBELN, EBELP\`
@@ -76,13 +270,17 @@ export const mmData = `
 
   * **EKPO** (Purchasing Document Item): su \`EBELN, EBELP\`
 
+---
+
 **VETVG (Delivery Due Index for Stock Transfer)**
 
-* Chiavi Primarie: \`VSTEL, LEDAT, LRRIO, ROUTE, SPDNR, WADAT, KUNWE,VBELN\`
+* Chiavi Primarie: \`VSTEL, LEDAT, LRRIO, ROUTE, SPDNR, WADAT, KUNWE, VBELN\`
 * Descrizione: Indice per le scadenze di consegna relative ai trasferimenti di stock.
 * Possibili Join:
 
   * **EKPO** (Purchasing Document Item): su \`EBELN, EBELP\`
+
+---
 
 **EKES (Order Acceptance/Fulfillment Confirmations)**
 
@@ -92,15 +290,18 @@ export const mmData = `
 
   * **EKPO** (Purchasing Document Item): su \`EBELN, EBELP\`
 
+---
+
 **EKKN (Account assignment in purchasing)**
 
 * Chiavi Primarie: \`EBELN, EBELP, ZEKKEN\`
 * Descrizione: Assegnazioni contabili nei documenti d'acquisto (es. centro di costo, ordine).
 * Possibili Join:
 
-  * **EKKO** (Purchasing Document Header): su \`EBELN\`
   * **EKPO** (Purchasing Document Item): su \`EBELN, EBELP\`
   * **EBKN** (Account Assignment in Purchase Requisition): su \`BANFN, BNFPO, ZEBEL\`
+
+---
 
 **EKAN (Vendor address purchasing)**
 
@@ -110,13 +311,17 @@ export const mmData = `
 
   * **EKKO** (Purchasing Document Header): su \`EBELN\`
 
+---
+
 **EKPA (Partner functions)**
 
-* Chiavi Primarie: \`EBELN, EBELP,EKORG,LTSNR,WERKS, PARVW,PARZA\`
+* Chiavi Primarie: \`EBELN, EBELP, EKORG, LTSNR, WERKS, PARVW, PARZA\`
 * Descrizione: Funzioni dei partner commerciali nel documento d'acquisto (es. fornitore, ricevitore merce).
 * Possibili Join:
 
   * **EKKO** (Purchasing Document Header): su \`EBELN\`
+
+---
 
 **EIPO (Item export / import data)**
 
@@ -125,6 +330,8 @@ export const mmData = `
 * Possibili Join:
 
   * **EKPO** (Purchasing Document Item): su \`EBELN, EBELP\`
+
+---
 
 **EINA (Purchasing Info Record - General Data)**
 
@@ -135,6 +342,8 @@ export const mmData = `
   * **EINE** (Purchasing Info Record - Purchasing Organization Data): su \`INFNR\`
   * **EKPO** (Purchasing Document Item): su \`INFNR\`
 
+---
+
 **EINE (Purchasing Info Record - Purchasing Organization Data)**
 
 * Chiavi Primarie: \`INFNR, EKORG, WERKS, ESOKZ\`
@@ -143,14 +352,19 @@ export const mmData = `
 
   * **EINA** (Purchasing Info Record - General Data): su \`INFNR\`
 
+---
+
 **EORD (Source list)**
 
 * Chiavi Primarie: \`MATNR, WERKS, ZEORD\`
 * Descrizione: Elenco fonti, definisce i fornitori validi per un materiale in uno specifico impianto.
 * Possibili Join:
 
+  * **EKPO** (Purchasing Document Item): su \`MATNR, WERKS, LIFNR\`
   * **MARA** (General Material Data): su \`MATNR\`
   * **LFA1** (Vendor Master - General Section): su \`LIFNR\`
+
+---
 
 **EBAN (Purchase Requisition)**
 
@@ -161,6 +375,8 @@ export const mmData = `
   * **EKPO** (Purchasing Document Item): su \`BANFN, BNFPO\`
   * **EBKN** (Account Assignment in Purchase Requisition): su \`BANFN, BNFPO\`
 
+---
+
 **EBKN (Account Assignment in Purchase Requisition)**
 
 * Chiavi Primarie: \`BANFN, BNFPO, ZEBEL\`
@@ -168,7 +384,7 @@ export const mmData = `
 * Possibili Join:
 
   * **EBAN** (Purchase Requisition): su \`BANFN, BNFPO\`
-  * **EKKN** (Account assignment in purchasing): su \`BANFN, BNFPO, ZEBEL\` 
+  * **EKKN** (Account assignment in purchasing): su \`BANFN, BNFPO, ZEBEL\`
 
 
 
